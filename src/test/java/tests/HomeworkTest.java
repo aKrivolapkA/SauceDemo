e120b7e46c45b0a7b9b25a88c2daf06f74c36ca9
Alema <akryvalap@gmail.com>
2024-08-02 20:34:39 +0200

    homework3

---
+++ src/test/java/tests/HomeworkTest.java
@@ -0,0 +1,53 @@
+package tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class HomeworkTest {
+
+    private WebDriver driver;
+    @FindBy(xpath ="//*[@id='user-name']" )
+    WebElement usernameInput;
+    @FindBy(xpath ="//*[@id='password']" )
+    WebElement passwordInput;
+
+    @FindBy(xpath ="//*[@id='login-button']" )
+    WebElement loginButton;
+
+
+    @BeforeMethod
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-search-engine-choice-screen");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.get("https://www.saucedemo.com");
+        PageFactory.initElements(driver, this);
+    }
+
+    @Test
+    public void loginSteps(){
+        usernameInput.sendKeys("standard_user");
+        passwordInput.sendKeys("secret_sauce");
+        loginButton.click();
+        String logo = driver.findElement(By.xpath("//*[@class='app_logo']")).getText();
+        Assert.assertEquals(logo,"Swag Labs");
+        driver.findElement(By.xpath("//*[@id ='add-to-cart-sauce-labs-backpack']")).click();
+        driver.findElement(By.xpath("//*[@id ='shopping_cart_container']")).click();
+        String nameItem = driver.findElement(By.xpath("//*[@id ='item_4_title_link']")).getText();
+        Assert.assertEquals(nameItem,"Sauce Labs Backpack");
+        String priceItem= driver.findElement(By.xpath("//*[@class ='inventory_item_price']")).getText();
+        Assert.assertEquals(priceItem,"$29.99");
+        driver.quit();
+    }
+}
