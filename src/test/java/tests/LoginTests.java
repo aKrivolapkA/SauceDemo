8da043878f6d9962fac0c39cbefa4b0f66254294
Alema <akryvalap@gmail.com>
2024-08-02 20:34:30 +0200

    commmit4

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,9 +1,5 @@
 package tests;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.FindBy;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -21,13 +17,13 @@ public class LoginTests extends BaseTest implements ITestConstants {
                 .login("", "");
         Assert.assertEquals(loginPage.getErrorMessageText(), EMPTY_USER_NAME_ERROR_TEXT);
     }
-    @Test(retryAnalyzer = Retry.class)
-    public void loginWithEmptyFieldsTestWithRetry() {
-        loginPage
-                .openPage()
-                .login("", "");
-        Assert.assertEquals(loginPage.getErrorMessageText(), EMPTY_USER_NAME_ERROR_TEXT_ERROR);
-    }
+   // @Test(retryAnalyzer = Retry.class)
+   // public void loginWithEmptyFieldsTestWithRetry() {
+   //     loginPage
+   //             .openPage()
+   //             .login("", "");
+   //     Assert.assertEquals(loginPage.getErrorMessageText(), EMPTY_USER_NAME_ERROR_TEXT_ERROR);
+   // }
 
     @Test
     public void loginWithEmptyUserNameFieldsTest() {
@@ -61,40 +57,5 @@ public class LoginTests extends BaseTest implements ITestConstants {
                 .login(USERNAME, PASSWORD);
         driver.getCurrentUrl();
         Assert.assertEquals(productsPage.getProductText(), "Products");
-    }
-
-    // в браузере делать разные действия
-    public void lo1() {
-        Actions actions = new Actions(driver);
-        actions.doubleClick();
-    }
-
-    // ниже отличие обычного использования и PageFactory
-    @FindBy(xpath = "//*[contains(text(),'Add E')]")
-    WebElement addButton;
-
-    @FindBy(xpath = "//*[contains(text(),'Delete')]")
-    WebElement removeButton;
-
-    @Test
-    public void addWithoutPageFactory() {
-        driver.get("https://the-internet.herokuapp.com/add_remove_elements/");
-        WebElement addButton = driver.findElement(By.xpath("//*[contains(text(),'Add E')]"));
-        addButton.click();
-        WebElement deleteButton = driver.findElement(By.xpath("//*[contains(text(),'Delete')]"));
-        deleteButton.click();
-        addButton.click();
-        // deleteButton.click(); //StaleElementReferenceException  кнопка вроде та же но нет
-    }
-
-    @Test
-    public void addWithPageFactory() {
-        driver.get("https://the-internet.herokuapp.com/add_remove_elements/");
-        WebElement addButtonPageFactory = addButton;
-        addButtonPageFactory.click();
-        WebElement deleteButtonPageFactory = removeButton;
-        deleteButtonPageFactory.click();
-        addButtonPageFactory.click();
-        deleteButtonPageFactory.click();
     }
 }
